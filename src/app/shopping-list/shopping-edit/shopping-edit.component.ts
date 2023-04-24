05baf3e13140905f34fc44793706495af27a5da2
marilena.buzatu <marilena.buzatu@asf.ro>
2023-04-24 11:22:35 +0300

    courseProject modificare addIngredients folosint Form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Ingredient} from "../../shared/ingredient.model";
 import {ShoppingListService} from "../shopping-list.service";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-shopping-edit',
@@ -9,16 +10,12 @@ import {ShoppingListService} from "../shopping-list.service";
 })
 export class ShoppingEditComponent implements OnInit{
 
-  @ViewChild('nameInput', {static: false}) nameInputRef: ElementRef;
-  @ViewChild('amountInput', {static: false}) amountInputRef: ElementRef;
-
   constructor(private shoppingService: ShoppingListService) {
   }
 
-  onAddItem() {
-    const newIngredient = new Ingredient(
-      this.nameInputRef.nativeElement.value,
-      this.amountInputRef.nativeElement.value);
+  onAddItem(form: NgForm) {
+    const newIngredient = new Ingredient(form.value.name,
+      form.value.amount);
     this.shoppingService.addIngredient(newIngredient);
   }
 
